60fd483b03c3480f5011331e6e61b0be5a9bae9c
Jonathan <164126841+jonathanch12@users.noreply.github.com>
2026-09-05 10:13:18 +0700

    Remove phone field from user model

--- models.py
+++ models.py
@@ -100,7 +100,6 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True, server_default=db.FetchedValue())
     name = db.Column(db.String(255), nullable=False)
     email = db.Column(db.String(255), nullable=False, unique=True)
-    phone = db.Column(db.String(255), nullable=False)
     password = db.Column(db.String(255), nullable=False)
     address = db.Column(db.Text, nullable=False)
     created_at = db.Column(db.DateTime, server_default=db.FetchedValue())
@@ -111,7 +110,6 @@ class User(db.Model):
             'id': self.id,
             'name': self.name,
             'email': self.email,
-            'phone': self.phone,
             'role': self.role,
             'created_at': self.created_at.isoformat() if self.created_at else None
         }
